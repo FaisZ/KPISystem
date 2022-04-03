41ffba6442ec1dff3f5318cff331e652ea9e94c8
Fais Azif <faisz9999@gmail.com>
2022-04-03 13:47:52 +0700

    kpi_group done

---
+++ database/migrations/2022_04_03_064205_create_kpi_group_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kpi_group', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->integer('level');
+            $table->unsignedBigInteger('parent_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kpi_group');
+    }
+};
