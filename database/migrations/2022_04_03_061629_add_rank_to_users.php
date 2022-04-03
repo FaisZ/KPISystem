80af395ea828310779cc055f43d2e793130fafe4
Fais Azif <faisz9999@gmail.com>
2022-04-03 13:21:11 +0700

    update migrations

---
+++ database/migrations/2022_04_03_061629_add_rank_to_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+          $table->index('rank_id');
+          $table->foreign('rank_id')->references('id')->on('rank')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+          $table->dropForeign('rank_id');
+          $table->dropIndex('rank_id');
+        });
+    }
+};
