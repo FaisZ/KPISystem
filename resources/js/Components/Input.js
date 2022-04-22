385939351f17064bdd729bd107bcf532ccc30f43
FaisZ <fais9999@ymail.com>
2022-04-22 08:14:12 +0700

    return input.js to default

--- resources/js/Components/Input.js
+++ resources/js/Components/Input.js
@@ -32,7 +32,7 @@ export default function Input({
                 ref={input}
                 autoComplete={autoComplete}
                 required={required}
-                //onChange={(e) => handleChange(e)}
+                onChange={(e) => handleChange(e)}
             />
         </div>
     );
