a3ef75c96680201444b5507c96463f65fce7d66c
FaisZ <fais9999@ymail.com>
2022-05-09 21:19:50 +0700

    add routes

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,9 @@ Route::post('/pegawai/add_aktivitas', [PegawaiController::class, 'addAktivitas']
 Route::post('/pegawai/edit_aktivitas', [PegawaiController::class, 'editAktivitas'])->middleware(['auth', 'verified'])->name('pegawai_edit_activity');
 Route::post('/pegawai/delete_aktivitas', [PegawaiController::class, 'deleteAktivitas'])->middleware(['auth', 'verified'])->name('pegawai_delete_activity');
 
-Route::get('/atasan', [AtasanController::class, 'index'])->middleware(['auth', 'verified'])->name('atasan');
-Route::post('/atasan/setuju', [AtasanController::class, 'acceptAktivitas'])->middleware(['auth', 'verified']);
-Route::post('/atasan/tolak', [AtasanController::class, 'rejectAktivitas'])->middleware(['auth', 'verified']);
+Route::get('/atasan', [AtasanController::class, 'index'])->middleware(['auth', 'atasan'])->name('atasan');
+Route::post('/atasan/setuju', [AtasanController::class, 'acceptAktivitas'])->middleware(['auth', 'atasan']);
+Route::post('/atasan/tolak', [AtasanController::class, 'rejectAktivitas'])->middleware(['auth', 'atasan']);
 
 // Route::get('/pegawai/add', [PegawaiController::class, 'addActivity']);
 // Route::post('e/addPegawai', [PegawaiController::class, 'addPegawai']);
@@ -50,18 +50,16 @@ Route::get('/dashboard', function () {
   return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/administrator/add_unsur', [AdministratorController::class, 'newUnsur'])->middleware(['auth', 'admin'])->name('add_unsur');
+Route::post('/administrator/add_unsur', [AdministratorController::class, 'addUnsur'])->middleware(['auth', 'admin'])->name('add_unsur');
 
-Route::get('/administrator', [AdministratorController::class, 'index'])->middleware(['auth', 'verified'])->name('administrator');
-Route::get('/administrator/add_unsur', [AdministratorController::class, 'newUnsur'])->middleware(['auth', 'verified'])->name('add_unsur');
-Route::post('/administrator/add_unsur', [AdministratorController::class, 'addUnsur'])->middleware(['auth', 'verified'])->name('add_unsur');
+Route::get('/administrator/aktivitas', [AdministratorController::class, 'listAktivitas'])->middleware(['auth', 'admin'])->name('master_aktivitas');
+Route::get('/administrator/add_aktivitas', [AdministratorController::class, 'newAktivitas'])->middleware(['auth', 'admin'])->name('add_aktivitas');
+Route::post('/administrator/add_aktivitas', [AdministratorController::class, 'addAktivitas'])->middleware(['auth', 'admin'])->name('add_aktivitas');
+Route::post('/administrator/update_aktivitas', [AdministratorController::class, 'updateAktivitas'])->middleware(['auth', 'admin'])->name('update_aktivitas');
 
-Route::get('/administrator/aktivitas', [AdministratorController::class, 'listAktivitas'])->middleware(['auth', 'verified'])->name('master_aktivitas');
-Route::get('/administrator/add_aktivitas', [AdministratorController::class, 'newAktivitas'])->middleware(['auth', 'verified'])->name('add_aktivitas');
-Route::post('/administrator/add_aktivitas', [AdministratorController::class, 'addAktivitas'])->middleware(['auth', 'verified'])->name('add_aktivitas');
-Route::post('/administrator/update_aktivitas', [AdministratorController::class, 'updateAktivitas'])->middleware(['auth', 'verified'])->name('update_aktivitas');
-
-Route::get('/administrator/pengguna', [AdministratorController::class, 'listUsers'])->middleware(['auth', 'verified'])->name('master_users');
-Route::post('/administrator/update_user', [AdministratorController::class, 'updateUser'])->middleware(['auth', 'verified']);
+Route::get('/administrator/pengguna', [AdministratorController::class, 'listUsers'])->middleware(['auth', 'admin'])->name('master_users');
+Route::post('/administrator/update_user', [AdministratorController::class, 'updateUser'])->middleware(['auth', 'admin']);
 
 Route::get('/e', function () {
   return Inertia::render('Experiments', ['hello' => 'world']);
