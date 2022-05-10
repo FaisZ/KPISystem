335af71a8f9925c6a47b3d380df9c28a14ace7da
FaisZ <fais9999@ymail.com>
2022-05-10 08:48:55 +0700

    ui fixes

--- resources/js/Layouts/Authenticated.js
+++ resources/js/Layouts/Authenticated.js
@@ -11,7 +11,9 @@ export default function Authenticated({ auth, header, children }) {
     return (
         <div className="min-h-screen bg-gray-100">
             <nav className="bg-white border-b border-gray-100">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
+                {/* Use this if wanted to be smaller */}
+                {/* <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"> */}
                     <div className="flex justify-between h-16">
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
@@ -142,7 +144,9 @@ export default function Authenticated({ auth, header, children }) {
 
             {header && (
                 <header className="bg-white shadow">
-                    <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">{header}</div>
+                {/* Use this if wanted to be smaller */}
+                {/* <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">{header}</div> */}
+                <div className="max-w-8xl mx-auto py-6 px-4 sm:px-6 lg:px-8">{header}</div>
                 </header>
             )}
 
--- resources/js/Pages/Dashboard.js
+++ resources/js/Pages/Dashboard.js
@@ -11,10 +11,11 @@ export default function Dashboard(props) {
         >
             <Head title="Dashboard" />
 
+        
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 bg-white border-b border-gray-200">You're logged in! {props.auth.user.total_score}</div>
+                        <div className="p-6 bg-white border-b border-gray-200">Total Perolehan Skor: {props.auth.user.total_score}</div>
                     </div>
                 </div>
             </div>
