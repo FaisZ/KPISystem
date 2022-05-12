ba8caaabc1b564fd102814eafb15ba486b3e3c91
aryahasburrahman <aryahasburrahman56@gmail.com>
2022-05-12 15:39:11 +0700

    footer done

---
+++ resources/js/Components/Footer.js
@@ -0,0 +1,53 @@
+import React from "react";
+import {
+  Box,
+  Container,
+  Row,
+  Column,
+  FooterLink,
+  Heading,
+} from "./FooterStyles";
+  
+const Footer = () => {
+  return (
+    <Box>
+      <Container>
+        <Row>
+          <Column>
+            <Heading>About Us</Heading>
+            <FooterLink href="#">Aim</FooterLink>
+            </Column>
+          <Column>
+            <Heading>Services</Heading>
+            <FooterLink href="#">Writing</FooterLink>
+            <FooterLink href="#">Internships</FooterLink>
+            </Column>
+          <Column>
+            <Heading>Contact Us</Heading>
+            <FooterLink href="#">Uttar Pradesh</FooterLink>
+            <FooterLink href="#">Ahemdabad</FooterLink>
+            </Column>
+          <Column>
+            <Heading>Social Media</Heading>
+            <FooterLink href="#">
+              <i className="fab fa-facebook-f">
+                <span style={{ marginLeft: "10px" }}>
+                  Facebook
+                </span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-instagram">
+                <span style={{ marginLeft: "10px" }}>
+                  Instagram
+                </span>
+              </i>
+            </FooterLink>
+            
+            </Column>
+        </Row>
+      </Container>
+    </Box>
+  );
+};
+export default Footer;
--- resources/js/Pages/PegawaiDashboard.js
+++ resources/js/Pages/PegawaiDashboard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function PegawaiDashboard(props) {
     return (
-        <div className="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+        <div className="mt-8 bg-white dark:bg-white-800 overflow-hidden shadow sm:rounded-lg">
         <div className="grid grid-cols-1 md:grid-cols-2">
             <div className="p-6">
                 <div className="flex items-center">
@@ -18,7 +18,7 @@ export default function PegawaiDashboard(props) {
                         <path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path>
                     </svg>
 
-                    <div className="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">
+                    <div className="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-black">
                         Total Perolehan Skor
                     </div>
                 </div>
@@ -41,17 +41,17 @@ export default function PegawaiDashboard(props) {
                         viewBox="0 0 24 24"
                         className="w-8 h-8 text-gray-500"
                     >
-                        <path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path>
-                        <path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                        <path d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"></path>
+                        {/* <path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path> */}
                     </svg>
 
-                    <div className="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">
+                    <div className="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-black">
                         Total Aktivitas Diajukan
                     </div>
                 </div>
 
                 <div className="ml-12">
-                    <div className="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                    <div className="mt-2 text-gray-600 dark:text-gray-400 text-black">
                         30 Aktivitas
                     </div>
                 </div>
@@ -68,16 +68,16 @@ export default function PegawaiDashboard(props) {
                         viewBox="0 0 24 24"
                         className="w-8 h-8 text-gray-500"
                     >
-                        <path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path>
+                        <path d="M5 13l4 4L19 7"></path>
                     </svg>
 
-                    <div className="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">
+                    <div className="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-black">
                         Aktivitas Disetujui
                     </div>
                 </div>
 
                 <div className="ml-12">
-                    <div className="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                    <div className="mt-2 text-gray-600 dark:text-gray-400 text-black">
                         30 Aktivitas
                     </div>
                 </div>
@@ -94,16 +94,16 @@ export default function PegawaiDashboard(props) {
                         viewBox="0 0 24 24"
                         className="w-8 h-8 text-gray-500"
                     >
-                        <path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                        <path d="M6 18L18 6M6 6l12 12"></path>
                     </svg>
 
-                    <div className="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">
+                    <div className="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-black">
                         Aktivitas Ditolak
                     </div>
                 </div>
 
                 <div className="ml-12">
-                    <div className="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                    <div className="mt-2 text-gray-600 dark:text-gray-400 text-black">
                         30 Aktivitas
                     </div>
                 </div>
