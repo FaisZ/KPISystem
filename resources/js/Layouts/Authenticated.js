efe98df84c8ceedcd14cf30656050c6669bbb2d3
FaisZ <fais9999@ymail.com>
2022-05-12 14:14:03 +0700

    add dropdowns

--- resources/js/Layouts/Authenticated.js
+++ resources/js/Layouts/Authenticated.js
@@ -83,16 +83,86 @@ export default function Authenticated({ auth, header, children, errors }) {
                                     Pengajuan Pribadi
                                 </NavLink>
                                 {
-                                    (auth.user.has_underling==1) ? (<>
-                                        <NavLink href={route('atasan')} active={route().current('atasan')}>
+                                    (auth.user.has_underling==1) ? (<div className="hidden sm:flex sm:items-center sm:ml-6">
+                                        <Dropdown>
+                                            <Dropdown.Trigger>
+                                                <span className="inline-flex rounded-md">
+                                                    <button
+                                                        type="button"
+                                                        className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                                    >
+                                                        Bawahan
+
+                                                        <svg
+                                                            className="ml-2 -mr-0.5 h-4 w-4"
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            viewBox="0 0 20 20"
+                                                            fill="currentColor"
+                                                        >
+                                                            <path
+                                                                fillRule="evenodd"
+                                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                                clipRule="evenodd"
+                                                            />
+                                                        </svg>
+                                                    </button>
+                                                </span>
+                                            </Dropdown.Trigger>
+
+                                            <Dropdown.Content>
+                                                <Dropdown.Link href={route('atasan')} method="get" as="button">
+                                                    Aktivitas Bawahan
+                                                </Dropdown.Link>
+                                            </Dropdown.Content>
+                                        </Dropdown>
+                                        {/* <NavLink href={route('atasan')} active={route().current('atasan')}>
                                             Aktivitas Bawahan
-                                        </NavLink>
-                                    </>
+                                        </NavLink> */}
+                                    </div>
                                     ) : (<></>)
                                 }
                                 {
-                                    (auth.user.is_admin==1) ? (<>
-                                        <NavLink href={route('master_aktivitas')} active={route().current('master_aktivitas')}>
+                                    (auth.user.is_admin==1) ? (<div className="hidden sm:flex sm:items-center sm:ml-6">
+                                        <Dropdown>
+                                            <Dropdown.Trigger>
+                                                <span className="inline-flex rounded-md">
+                                                    <button
+                                                        type="button"
+                                                        className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                                    >
+                                                        Administrator
+
+                                                        <svg
+                                                            className="ml-2 -mr-0.5 h-4 w-4"
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            viewBox="0 0 20 20"
+                                                            fill="currentColor"
+                                                        >
+                                                            <path
+                                                                fillRule="evenodd"
+                                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                                clipRule="evenodd"
+                                                            />
+                                                        </svg>
+                                                    </button>
+                                                </span>
+                                            </Dropdown.Trigger>
+
+                                            <Dropdown.Content>
+                                                <Dropdown.Link href={route('master_aktivitas')} method="get" as="button">
+                                                    Kelola Aktivitas
+                                                </Dropdown.Link>
+                                                <Dropdown.Link href={route('master_users')} method="get" as="button">
+                                                    Kelola Pengguna
+                                                </Dropdown.Link>
+                                                <Dropdown.Link href={route('register')} method="get" as="button">
+                                                    Daftarkan Pengguna
+                                                </Dropdown.Link>
+                                            </Dropdown.Content>
+                                        </Dropdown>
+
+
+                                        {/* <NavLink href={route('master_aktivitas')} active={route().current('master_aktivitas')}>
                                             Kelola Aktivitas
                                         </NavLink>
                                         <NavLink href={route('master_users')} active={route().current('master_users')}>
@@ -100,8 +170,8 @@ export default function Authenticated({ auth, header, children, errors }) {
                                         </NavLink>
                                         <NavLink href={route('register')} active={route().current('register')}>
                                             Daftarkan Pengguna
-                                        </NavLink>
-                                    </>
+                                        </NavLink> */}
+                                    </div>
                                     ) : (<></>)
                                 }
                             </div>
