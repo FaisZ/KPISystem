d0039d7d6db34a8d0bf1d6d9fcf3b32cc1e4d133
FaisZ <fais9999@ymail.com>
2022-05-19 12:22:46 +0700

    add nullable nip

--- database/migrations/2022_05_19_051736_add_nip_to_users.php
+++ database/migrations/2022_05_19_051736_add_nip_to_users.php
@@ -13,14 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('nip')->unique()->nullable();
         });
     }
 
@@ -31,6 +25,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('nip');
+        });
     }
 };
