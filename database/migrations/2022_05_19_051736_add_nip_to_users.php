e435a5035d199042e0e02caa414d1d63f3408255
FaisZ <fais9999@ymail.com>
2022-05-27 22:02:34 +0700

    change nip to string to enable 18 digits

--- database/migrations/2022_05_19_051736_add_nip_to_users.php
+++ database/migrations/2022_05_19_051736_add_nip_to_users.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('nip')->unique()->nullable();
+            $table->string('nip')->unique()->nullable();
         });
     }
 
